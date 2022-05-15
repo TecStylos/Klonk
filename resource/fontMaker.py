b34798dafed257f3d3e696576695cc1f66afb171
TecStylos <tecstylos@gmail.com>
2022-05-15 13:35:05 +0200

    Added font resources

---
+++ resource/fontMaker.py
@@ -0,0 +1,3 @@
+import pygame
+pygame.font.init()
+pygame.image.save(pygame.font.SysFont("cascadiamono", 100).render(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~", 0, pygame.Color(255, 255, 255), pygame.Color(0, 0, 0)), "image.jpg")
