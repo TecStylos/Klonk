dc3ea14e34b289a155440a34380f436303fe0886
TecStylos <tecstylos@gmail.com>
2023-07-04 22:20:47 +0200

    Fixed image rendering issue

--- src/ui/UserInterface.cpp
+++ src/ui/UserInterface.cpp
@@ -84,16 +84,23 @@ void UISpace::remElement(UIElement* pElem)
 UIImage::UIImage(int x, int y, int w, int h)
 	: UIElement(x, y, w, h), m_img(w, h)
 {
+	initCallbacks();
+}
+
+UIImage::UIImage(int x, int y, Image& img)
+	: UIElement(x, y, img.width(), img.height()), m_img(img)
+{
+	initCallbacks();
+}
+
+void UIImage::initCallbacks()
+{
 	setCbOnRender(
 		[](const UIElement* pThis, Framebuffer& fb, const void* pData) {
 			fb.drawImage(((const UIImage*)pThis)->m_x, ((const UIImage*)pThis)->m_y, ((const UIImage*)pThis)->m_img);
 		}
 	);
 }
-
-UIImage::UIImage(int x, int y, Image& img)
-	: UIElement(x, y, img.width(), img.height()), m_img(img)
-{}
 
 Image& UIImage::getImage()
 {
--- src/ui/UserInterface.h
+++ src/ui/UserInterface.h
@@ -74,6 +74,8 @@ class UIImage : public UIElement
 public:
 	UIImage(int x, int y, int w, int h);
 	UIImage(int x, int y, Image& img);
+private:
+	void initCallbacks();
 public:
 	Image& getImage();
 protected:
